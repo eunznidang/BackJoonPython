8c3d064086e3f0edc93cd9d2947097b637f6f139
Eunjin Lee <ejejnin@gmail.com>
2025-01-21 21:07:26 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 740 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,9 @@
+a, b, c, d, e, f=map(int, input().split())
+answer=0
+
+
+for x in range(-999, 1000, 1):
+    for y in range(-999, 1000, 1):
+        if a*x+b*y == c and d*x+e*y==f:
+            print(x, y)
+            break

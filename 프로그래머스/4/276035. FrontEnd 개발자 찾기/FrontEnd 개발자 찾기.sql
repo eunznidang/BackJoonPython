4c52989ff2bac44ef31826652979af4a274e6529
Eunjin Lee <ejejnin@gmail.com>
2025-03-13 16:46:42 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,30 +1,13 @@
-# select id
-# , email
-# , first_name
-# , last_name
+select distinct(id)
+, email
+, first_name
+, last_name
 
-# from developers as d
-# join (
-# select * from skillcodes
-#     where category='Front End'
-# )as s
-# on d.skill_code&s.code=s.code
+from developers as d
+join (
+select * from skillcodes
+    where category='Front End'
+)as s
+on d.skill_code & s.code = s.code
 
-# order by id
-
-WITH TBL AS (
-    SELECT SUM(CODE) AS SUM_CODE
-    FROM SKILLCODES
-    WHERE CATEGORY = 'Front End'
-)
-
-SELECT
-    d.ID,
-    d.EMAIL,
-    d.FIRST_NAME,
-    d.LAST_NAME
-FROM
-    DEVELOPERS d INNER JOIN TBL t
-    ON d.SKILL_CODE & t.SUM_CODE > 0
-ORDER BY
-    d.ID ASC;
+order by id
